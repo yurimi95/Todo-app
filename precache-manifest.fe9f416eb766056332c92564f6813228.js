97be53e4e70bc3790820c7d4bd63c93c85235e5d
soo054141 <48131318+soo054141@users.noreply.github.com>
2020-10-06 19:43:05 +0900

    Updates

---
+++ precache-manifest.fe9f416eb766056332c92564f6813228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46adc7a411fdfd59621f77e20d26ad35",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "d1751ebc39b16c05abfe",
+    "url": "/Todo-app/static/js/2.18905c25.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-app/static/js/2.18905c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbdd8314032bbb7f08a0",
+    "url": "/Todo-app/static/js/main.77f24b0c.chunk.js"
+  },
+  {
+    "revision": "1eb6d6ec195e648776ea",
+    "url": "/Todo-app/static/js/runtime-main.375335ae.js"
+  }
+]);
